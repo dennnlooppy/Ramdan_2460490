6737e181e2e567ca1ba4702c67fad4692f6f1b39
dennnlooppy <ramdanabdulazis0@gmail.com>
2025-07-16 12:07:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-import streamlit as st
-
-st.write("Hello, *World!* :sunglasses:")
-import streamlit as st
-
-import streamlit as st
-
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+Nama indikator,Warna Asam,Warna Basa,Rentang pH
+asam pikrat, tak berwarna, kunging, 0,1- 0,8
+2,6 dinitro fenol, tak berwarna, kuning, 2,0-4,0
+Metil Jingga,Merah,Kuning,3.1 - 4.4
+hijau brom kresol, kuning, biru, 3,8 - 5,4
+pur pur brom kresol, kuning, ungu, 5,2 - 6,8
+Metil Merah,Merah,Kuning,4.4 - 6.2
+merah fenol, kuning, merah, 6,4 - 8,0
+Bromtimol Biru,Kuning,Biru,6.0 - 7.6
+A-naftolftalein, kuning, biru, 7,0 - 9,0
+pur pur kresol, kuning, biru, 7,4 - 9,6
+timol ftalein, tak berwarna, biru, 9,3 - 10,5
+Fenolftalein,Tidak berwarna,Merah Muda,8.3 - 10.0
+Timol Biru,Merah,Biru,1.2 - 2.8
+Lakmus,Merah,Biru,4.5 - 8.3
+Alizarin Kuning,rose,Oranye,10.1 - 12.0
+1,3,5 trinito benzen, tak berwarna, jingga, 12,0 - 14,0
+erio black tea, merah, biru, 6,3 - 11,5
